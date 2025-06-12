328fd01626f12500dfc814866ccdeea4d21b9c6d
phanivemula4 <vphaniprabhas@gmail.com>
2025-06-13 02:02:44 +0530

    new adds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 required to expose to Docker host
+    port: 5173,
+    watch: {
+      usePolling: true, // 👈 sometimes needed for Docker in Windows
+    }
+  }
 })
